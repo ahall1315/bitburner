a47a049825cea0ec3f8265b06255efa1497c7484
Alex Hall <ahall2@augusta.edu>
2022-08-22 05:04:33 -0400

    Added divide threads to hack_multiple.js

--- gameRoot/scripts/hack_multiple.js
+++ gameRoot/scripts/hack_multiple.js
@@ -6,31 +6,43 @@ export async function main(ns) {
 	var printTargets = "";
 	let host = ns.getHostname();
 	const noPrintSwitch = "-n";
+	const divideThreadsSwitch = "-r";
+	var threads = -1;
+	var confirm = false;
 
 	if (ns.args[0] == undefined) {
-		ns.tprint("Invalid usage. Provide servers to target as argument [server].")
+		ns.tprint("Invalid usage. Provide servers to target as argument [server]. Switch '-r' to divide threads among targets.")
 	} else {
 
-		var threads = await ns.prompt("How many threads?", { type: "text" });
-
+		threads = await ns.prompt("How many threads?", { type: "text" });
+	
 		if (isNaN(threads) || threads <= 0) {
 			ns.alert("Input for threads was not a number or was less than or equal to zero");
 		} else {
-
+			
 			for (let i = 0; i < ns.args.length; i++) {
-				targets.push(ns.args[i]);
+				if (ns.args[i] != divideThreadsSwitch) {
+					targets.push(ns.args[i]);
+				}
+			}
+			
+			// Divide the the given threads amongst the targets
+			if (ns.args.includes(divideThreadsSwitch)) {
+				threads = threads / targets.length;
 			}
 
 			for (let i = 0; i < targets.length; i++) {
-				printTargets = printTargets.concat("- " + targets[i] + "\n");
+				if (ns.args[i] != divideThreadsSwitch) {
+					printTargets = printTargets.concat("- " + targets[i] + "\n");
+				}
 			}
-
-			var confirm = await ns.prompt(`Hacking these servers with ${threads} threads:\n${printTargets} \nConfirm?`, { type: "boolean" });
-
+			
+			confirm = await ns.prompt(`Hacking these servers with ${threads} threads each:\n${printTargets} \nConfirm?`, { type: "boolean" });
 
 			if (confirm) {
 				for (let i = 0; i < targets.length; i++) {
 					ns.run("/scripts/open_ports.js", 1, targets[i], noPrintSwitch);
+					await ns.sleep(250); // Waits for ports to be opened
 					if (ns.isRunning("/scripts/hack.js", host, targets[i])) {
 						ns.print("Hack script already running for target: " + targets[i] + "! Killing script.")
 						ns.kill("/scripts/hack.js", host, targets[i])
