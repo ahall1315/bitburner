ee1aba80ebae086d39fd14267dc84cceaeee0405
Alex Hall <ahall2@augusta.edu>
2022-08-22 08:38:47 -0400

    Improved kill_scripts.js

--- gameRoot/control/kill_scripts.js
+++ gameRoot/control/kill_scripts.js
@@ -6,20 +6,48 @@ export async function main(ns) {
 	let servers = ns.getPurchasedServers();
 	let error = false;
 	let killed = false;
-
-	ns.tprint("Attempting to kill all scripts on purchased servers...");
-
-	for (var i = 0; i < servers.length; ++i) {
-		killed = ns.killall(hostPrefix + i);
-		if (!killed) {
-			error = true;
+	let count = 0;
+	let targets = [];
+	
+	// If there are no arguments
+	if (ns.args.length === 0) {
+		ns.tprint("Attempting to kill all scripts on purchased servers...");
+		
+		for (var i = 0; i < servers.length; ++i) {
+			killed = ns.killall(hostPrefix + i);
+			if (!killed) {
+				error = true;
+			} else {
+				count++;
+			}
 		}
-	}
+		
+		if (error) {
+			ns.tprint("There was an error in killing the scripts on one or more purchased servers.");
+		}
+		ns.tprint("Scripts successfully killed on " + count + " purchased server(s).");
 
-	if (!error) {
-		ns.tprint("Scripts successfully killed on all purchased servers");
 	} else {
-		ns.tprint("There was an error in killing the scripts.");
-	}
+		ns.tprint("Attempting to kill scripts on targets...");
 
+		for (i = 0; i < ns.args.length; i++) {
+			targets.push(ns.args[i]);
+		}
+
+		for (i = 0; i < targets.length; i++) {
+			killed = ns.killall(targets[i]);
+			if (!killed) {
+				error = true;
+			} else {
+				count++;
+			}
+		}
+
+		if (error) {
+			ns.tprint("There was an error in killing the scripts on one or more targets.");
+		}
+		ns.tprint("Scripts successfully killed on " + count + " target(s).");
+
+	}
+	
 }
