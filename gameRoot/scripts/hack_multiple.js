7d426646fa308e4ee2ce834916dc9726cd425981
Alex Hall <ahall2@augusta.edu>
2022-08-23 01:14:29 -0400

    Improved hack_multiple.js

--- gameRoot/scripts/hack_multiple.js
+++ gameRoot/scripts/hack_multiple.js
@@ -9,27 +9,29 @@ export async function main(ns) {
 	const divideThreadsSwitch = "-r";
 	var threads = -1;
 	var confirm = false;
+	let pid = -1;
+	let error = false;
 
 	if (ns.args[0] == undefined) {
 		ns.tprint("Invalid usage. Provide servers to target as argument [server]. Switch '-r' to divide threads among targets.")
 	} else {
 
 		threads = await ns.prompt("How many threads?", { type: "text" });
-	
+
 		if (isNaN(threads) || threads <= 0) {
 			ns.alert("Input for threads was not a number or was less than or equal to zero");
 		} else {
-			
+
 			for (let i = 0; i < ns.args.length; i++) {
 				if (ns.args[i] != divideThreadsSwitch) {
 					targets.push(ns.args[i]);
 				}
 			}
-			
+
 			// Divide the the given threads amongst the targets
 			if (ns.args.includes(divideThreadsSwitch)) {
 				threads = threads / targets.length;
-				threads = threads.toFixed(0);
+				threads = Math.floor(threads);
 			}
 
 			for (let i = 0; i < targets.length; i++) {
@@ -37,7 +39,7 @@ export async function main(ns) {
 					printTargets = printTargets.concat("- " + targets[i] + "\n");
 				}
 			}
-			
+
 			confirm = await ns.prompt(`Hacking these servers with ${threads} threads each:\n${printTargets} \nConfirm?`, { type: "boolean" });
 
 			if (confirm) {
@@ -48,18 +50,23 @@ export async function main(ns) {
 						ns.print("Hack script already running for target: " + targets[i] + "! Killing script.")
 						ns.kill("/scripts/hack.js", host, targets[i])
 					}
-					ns.run("/scripts/hack.js", threads, targets[i]);
+					pid = ns.run("/scripts/hack.js", threads, targets[i]);
+
+					if (pid <= 0) {
+						error = true;
+					}
+				}
+				if (targets.length === 1) {
+					ns.tprint("Hacking server...")
+				} else {
+					ns.tprint("Hacking multiple servers...");
+				}
+				if (error) {
+					ns.tprint("There was an error in hacking one or more servers. Is there enough RAM?");
 				}
 			}
 
-			if (targets.length === 1) {
-				ns.tprint("Hacking server...")
-			} else {
-				ns.tprint("Hacking multiple servers...");
-			}
-
 		}
-
 
 	}
 
