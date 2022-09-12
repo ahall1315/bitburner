5875ea455bd9b77c022ff35f06a9219df8f89ea4
Alex Hall <ahall2@augusta.edu>
2022-09-12 01:30:44 -0400

    Update comment

--- gameRoot/scripts/karma.js
+++ gameRoot/scripts/karma.js
@@ -1,4 +1,4 @@
-// Prints the player's karma to the terminal
+// Prints the player's karma to the terminal (Requirement to start gang: -54,000)
 
 /** @param {NS} ns */
 export async function main(ns) {
