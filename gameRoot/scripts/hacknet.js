ee974698230590c5d408574ff5e1ddf09fb196e6
Alex Hall <ahall2@augusta.edu>
2022-09-12 03:34:28 -0400

    Automated leveling

--- gameRoot/scripts/hacknet.js
+++ gameRoot/scripts/hacknet.js
@@ -5,6 +5,9 @@ export async function main(ns) {
     let error = false;
     let targetNode = -1;
     let numNodes = ns.hacknet.numNodes();
+    let playerMoney = -1;
+    let cost = -1;
+    const purchaseThresh = 10 / 100; // This value is a threshold between node cost and total money as a percentage
     const maxNodes = ns.hacknet.maxNumNodes(); // This value should be infinity
 
     ns.tprint("Beginning automated hacknet manager...");
@@ -14,29 +17,68 @@ export async function main(ns) {
     timeOnline is in seconds
     production is in dollars
     */
-    let nodeStats = ns.hacknet.getNodeStats(0);
+    //let nodeStats = ns.hacknet.getNodeStats(0);
 
-    //ns.tprint(JSON.stringify(nodeStats, null, 2));
-
-    //ns.hacknet.purchaseNode();
-
-    // const {production, purchaseCost} = ns.getHacknetMultipliers();
-    // ns.tprint(production);
-    // ns.tprint(purchaseCost);
-
-    
-    // calculate the return on investment for upgrading level, and if good enough, upgrade
+    // buy/upgrade a hacknet node if the cost is beneath a threshold
     while (true) {
-        for (let i = 0; i < numNodes; i++) {
-            targetNode = i;
-            error = !ns.hacknet.upgradeLevel(targetNode, 1);
+        playerMoney = ns.getPlayer().money;
+
+        // Purchasing a node
+        cost = ns.hacknet.getPurchaseNodeCost();
+        if (cost < (playerMoney * purchaseThresh)) {
+            targetNode = ns.hacknet.purchaseNode();
+            if (targetNode === -1) {
+                error = true;
+            }
+
             if (error) {
-                ns.print("There was an error in upgrading the level of a hacknet node.");
+                ns.print("There was an error in purchasing a hacknet node.");
                 error = false; // Reset error flag
             } else {
-                ns.print("Successfully upgraded a hacknet node");
+                ns.print("Successfully purchased a hacknet node with index: " + targetNode);
             }
         }
-        await ns.sleep(10000) // Wait ten seconds
+
+        for (let i = 0; i < numNodes; i++) {
+            targetNode = i;
+            playerMoney = ns.getPlayer().money;
+
+            // Upgrading the level
+            cost = ns.hacknet.getLevelUpgradeCost(targetNode, 1);
+            if (cost < (playerMoney * purchaseThresh)) {
+                error = !ns.hacknet.upgradeLevel(targetNode, 1);
+                if (error) {
+                    ns.print("There was an error in upgrading the level of hacknet-node-" + targetNode);
+                    error = false; // Reset error flag
+                } else {
+                    ns.print("Successfully upgraded the level of hacknet-node-" + targetNode);
+                }
+            }
+
+            // Upgrading the RAM
+            cost = ns.hacknet.getRamUpgradeCost(targetNode, 1);
+            if (cost < (playerMoney * purchaseThresh)) {
+                error = !ns.hacknet.upgradeRam(targetNode, 1);
+                if (error) {
+                    ns.print("There was an error in upgrading the RAM of hacknet-node-" + targetNode);
+                    error = false; // Reset error flag
+                } else {
+                    ns.print("Successfully upgraded the RAM of hacknet-node-" + targetNode);
+                }
+            }
+
+            // Upgrading the cores
+            cost = ns.hacknet.getCoreUpgradeCost(targetNode, 1);
+            if (cost < (playerMoney * purchaseThresh)) {
+                error = !ns.hacknet.upgradeCore(targetNode, 1);
+                if (error) {
+                    ns.print("There was an error in upgrading the cores of hacknet-node-" + targetNode);
+                    error = false; // Reset error flag
+                } else {
+                    ns.print("Successfully upgraded the cores of hacknet-node-" + targetNode);
+                }
+            }
+        }
+        await ns.sleep(5000) // Wait five seconds
     }
 }
