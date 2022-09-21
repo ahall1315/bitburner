a0667d93e78757d4a8f282bf9af2b79bd0bfa17b
Alex Hall <ahall2@augusta.edu>
2022-09-21 07:33:30 -0400

    Added destroy_bn.js

---
+++ gameRoot/scripts/destroy_bn.js
@@ -0,0 +1,17 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    let bn = ns.args[0];
+    let confirm = false;
+
+    if (bn === undefined) {
+        ns.tprint("WARN Incorrect usage. Please provide the [number] of the bitnode to jump to.");
+    } else {
+        confirm = await ns.prompt("Are you sure you want to destroy w0r1d_d43m0n and jump to BN" + bn + "?");
+
+        if (confirm) {
+            ns.singularity.destroyW0r1dD43m0n(bn);
+        }
+
+    }
+
+}
