ca2245c586310cb1a3c9a387aa815fb0bc2ed488
Alex Hall <ahall2@augusta.edu>
2022-09-22 01:12:05 -0400

    Prints percent

--- gameRoot/scripts/karma.js
+++ gameRoot/scripts/karma.js
@@ -2,5 +2,8 @@
 
 /** @param {NS} ns */
 export async function main(ns) {
-    ns.tprint("Karma level: " + ns.heart.break().toFixed(0));
+    const gangRequirement = -54000;
+    let karma = ns.heart.break();
+
+    ns.tprint("Karma level: " + karma.toFixed(0) + " (" + ((karma / gangRequirement) * 100).toFixed(2) + "% of gang requirement)");
 }
