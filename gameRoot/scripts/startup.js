8ef6961ae06806f3a327d24a8cf0b1c027b9a26b
Alex Hall <ahall2@augusta.edu>
2022-10-03 13:33:38 -0400

    Improved startup.js

--- gameRoot/scripts/startup.js
+++ gameRoot/scripts/startup.js
@@ -3,20 +3,79 @@
 /** @param {import("NetscriptDefinitions").NS} ns */
 export async function main(ns) {
     let pid = -1;
+    let totalRAM = 0;
+    let allFactions = false;
+    let error = false;
+    const toJoin = [
+        "Sector-12"
+    ]
+    const toRun = [
+        {
+            script: "/scripts/custom_stats.js",
+            args: null
+        },
+        {
+            script: "/scripts/buy_programs.js",
+            args: null
+        },
+        {
+            script: "/scripts/join_factions.js",
+            args: null
+        },
+        {
+            script: "/scripts/assign_sleeves.js",
+            args: null
+        },
+        {
+            script: "/scripts/hack_all.js",
+            args: ["--killHack"]
+        }
+    ]
 
-    ns.run("/scripts/custom_stats.js");
-
-    ns.run("/scripts/join_factions.js", 1, "-a");
-
-    pid = ns.run("/scripts/buy_programs.js");
-
-    while(ns.isRunning(pid)) {
-        ns.print("Waiting for script with pid " + pid + "to stop running");
-        await ns.sleep(500);
+    totalRAM += ns.getScriptRam(ns.getScriptName());
+    for (let i = 0; i < toRun.length; i++) {
+        totalRAM += ns.getScriptRam(toRun[i].script);
     }
-    pid = -1; // Reset pid
+    if (totalRAM >= ns.getServerMaxRam(ns.getHostname())) {
+        ns.tprint("WARN Not enough RAM to run all scripts! You need at least " + Math.ceil(totalRAM) + "GB of RAM to run all scripts.");
+    }
 
-    ns.run("/scripts/assign_sleeves.js");
+    for (let i = 0; i < toRun.length; i++) {
+        try {
+            if (toRun[i].args === null) {
+                pid = ns.run(toRun[i].script);
+            } else {
+                pid = ns.run(toRun[i].script, 1, ...toRun[i].args);
+            }
+            if (pid === 0) {
+                throw `ERROR Failed to run ${toRun[i].script}`;
+            }
 
-    ns.run("/scripts/hack_all.js", 1, "--killHack");
+            if (toRun[i] === "scripts/join_factions.js") {
+                // Waits for factions to be joined
+                while (!allFactions) {
+                    let count = 0;
+                    for (let i = 0; i < toJoin.length; i++) {
+                        if (ns.getPlayer().factions.includes(toJoin[i])) {
+                            count++;
+                        }
+                    }
+                    if (count === toJoin.length) {
+                        allFactions = true;
+                    }
+                    await ns.sleep(500);
+                }
+            }
+
+        } catch (err) {
+            error = true;
+            ns.print(err);
+        }
+
+    }
+
+    if (error) {
+        ns.tprint("ERROR One or more scripts failed to run");
+    }
+
 }
