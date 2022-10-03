fe25ba98cb70ac94fa4cd38f58b573293850a325
Alex Hall <ahall2@augusta.edu>
2022-10-03 13:50:46 -0400

    Added manual_hack.js

---
+++ gameRoot/scripts/manual_hack.js
@@ -0,0 +1,9 @@
+/** @param {import("NetscriptDefinitions").NS} ns */
+export async function main(ns) {
+    let money = 0;
+
+    while (true) {
+        money += await ns.singularity.manualHack();
+        ns.print("Manual hack has made: " + ns.nFormat(money, "$0.000a"));
+    }
+}
