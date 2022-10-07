83f8d2a16d82abdf6e73906fdbc971273f533f1a
Alex Hall <ahall2@augusta.edu>
2022-10-06 22:34:06 -0400

    use lib and commas

--- gameRoot/scripts/karma.js
+++ gameRoot/scripts/karma.js
@@ -1,10 +1,11 @@
 // Prints the player's karma to the terminal (Requirement to start gang: -54,000)
 // Karma persists between soft resets
 
+import { gangKarmaRequirement } from "lib/const";
+
 /** @param {import("NetscriptDefinitions").NS} ns */
 export async function main(ns) {
-    const gangRequirement = -54000;
     let karma = ns.heart.break();
 
-    ns.tprint("Karma level: " + karma.toFixed(0) + " (" + ((karma / gangRequirement) * 100).toFixed(2) + "% of gang requirement)");
+    ns.tprint("Karma level: " + ns.nFormat(karma.toFixed(0), "0,0") + " (" + ((karma / gangKarmaRequirement) * 100).toFixed(2) + "% of gang requirement)");
 }
