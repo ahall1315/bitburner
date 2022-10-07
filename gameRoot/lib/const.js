1e82cf08bd1d2bbefe0b8393c9400f46450dc638
Alex Hall <ahall2@augusta.edu>
2022-10-06 21:29:56 -0400

    Added const.js

---
+++ gameRoot/lib/const.js
@@ -0,0 +1,10 @@
+// Library for constants
+
+/** @param {import("NetscriptDefinitions").NS} ns */
+export const portPrograms = [
+    "BruteSSH.exe",
+    "FTPCrack.exe",
+    "relaySMTP.exe",
+    "HTTPWorm.exe",
+    "SQLInject.exe"
+]
