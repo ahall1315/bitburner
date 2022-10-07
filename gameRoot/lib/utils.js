f599f658c1119fc9f6214b8fdb3c7471402d500d
Alex Hall <ahall2@augusta.edu>
2022-10-06 21:30:20 -0400

    Added getNumOwnedPortPrograms

--- gameRoot/lib/utils.js
+++ gameRoot/lib/utils.js
@@ -1,5 +1,7 @@
 // Library for bitburner scripts. All exported functions must have ns as a parameter.
 
+import * as constants from "lib/const.js";
+
 /** @param {import("NetscriptDefinitions").NS} ns */
 export async function main(ns) {
     ns.tprint("This script functions as a library which stores utilities for other scripts.");
@@ -10,4 +12,17 @@ export function getRandomInt(ns, min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+/** @param {import("NetscriptDefinitions").NS} ns */
+export function getNumOwnedPortPrograms(ns) {
+    let count = 0;
+
+    for (let i = 0; i < constants.portPrograms.length; i++) {
+        if (ns.fileExists(constants.portPrograms[i])) {
+            count++;
+        }
+    }
+
+    return count;
 }
