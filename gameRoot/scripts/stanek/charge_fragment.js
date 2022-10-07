9dd943076b47e52fb5e7a2e53ecc8d556d16aa35
Alex Hall <ahall2@augusta.edu>
2022-10-07 12:27:51 -0400

    Loop through active fragments

--- gameRoot/scripts/stanek/charge_fragment.js
+++ gameRoot/scripts/stanek/charge_fragment.js
@@ -1,6 +1,22 @@
 /** @param {import("NetscriptDefinitions").NS} ns */
 export async function main(ns) {
+    const args = ns.flags([["help", false]]);
+    const rootX = args._[0];
+    const rootY = args._[1];
+
     while (true) {{
-        await ns.stanek.chargeFragment(0, 3);
+        let fragments = ns.stanek.activeFragments();
+
+        for (let i = 0; i < fragments.length; i++) {
+            if (fragments[i].limit === 99) {
+                fragments[i].isBooster = true;
+            } else {
+                fragments[i].isBooster = false;
+            }
+
+            if (!fragments[i].isBooster) {
+                await ns.stanek.chargeFragment(fragments[i].x, fragments[i].y);
+            }
+        }
     }}
 }
