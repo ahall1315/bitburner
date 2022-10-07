d79f80a5d2484f698fe57dd4aac9ee344b8daafc
Alex Hall <ahall2@augusta.edu>
2022-10-07 18:32:51 -0400

    Added sort

--- gameRoot/scripts/infiltrate.js
+++ gameRoot/scripts/infiltrate.js
@@ -1,47 +1,105 @@
 // Writes info about infiltration locations to a file
+// TODO: Is the reputation reward that is printed accurate? Need to confirm this.
 
 /** @param {import("NetscriptDefinitions").NS} ns */
 export async function main(ns) {
     let locations = ns.infiltration.getPossibleLocations();
     let infiltrations = [];
-    let infiltrationPath = "/serverinfo/infiltration.txt";
+    let infiltrationPath = "/data/infiltration.txt";
     let difficultySwitch = "-d";
     let writeString = "";
+
+    let args = ns.flags([["help", false], ["sort", ""]])
+
+    if (args.help) {
+        ns.tprint("This script will print info about infiltration locations and save the info to a file.");
+        ns.tprint("Option argument --sort to sort the info by a provided property.");
+        ns.tprint(`Usage: run ${ns.getScriptName()}`);
+        ns.tprint("Example 1:");
+        ns.tprint(`> run ${ns.getScriptName()}`);
+        ns.tprint("Example 2:");
+        ns.tprint(`> run ${ns.getScriptName()} --sort difficulty`);
+        return;
+    }
 
     for (let i = 0; i < locations.length; i++) {
         infiltrations.push(ns.infiltration.getInfiltration(locations[i].name));
     }
 
+    if (!["difficulty", "money", "cash", "reputation", "rep", ""].includes(args.sort.toLocaleLowerCase())) {
+        ns.tprint("WARN You may only sort by difficulty, money, and reputation.");
+        return;
+    }
+    
+    // Sort the infiltration locations by difficulty in descending order
+    if (args.sort === "difficulty") {
+        infiltrations.sort((a, b) => {
+            if (a.difficulty < b.difficulty) {
+                return 1;
+            }
+            if (a.difficulty > b.difficulty) {
+                return -1;
+            }
+            return 0;
+        })
+    }
+
+    // Sort the infiltration locations by money in descending order
+    if (args.sort === "money" || args.sort === "cash") {
+        infiltrations.sort((a, b) => {
+            if (a.reward.sellCash < b.reward.sellCash) {
+                return 1;
+            }
+            if (a.reward.sellCash > b.reward.sellCash) {
+                return -1;
+            }
+            return 0;
+        })
+    }
+
+    // Sort the infiltration locations by reputation in descending order
+    if (args.sort === "reputation" || args.sort === "reputation") {
+        infiltrations.sort((a, b) => {
+            if (a.reward.tradeRep < b.reward.tradeRep) {
+                return 1;
+            }
+            if (a.reward.tradeRep > b.reward.tradeRep) {
+                return -1;
+            }
+            return 0;
+        })
+    }
+
     if (ns.args.includes(difficultySwitch)) {
         for (let i = 0; i < infiltrations.length; i++) {
-            ns.tprint(infiltrations[i].location.name);
-            ns.tprint("    City: " + infiltrations[i].location.city);
-            ns.tprint("      Difficulty");
-            ns.tprint("          Difficulty level: " + (infiltrations[i].difficulty * 33.3333).toFixed(0) + " / 100");
-            ns.tprint("          Max clearance level: " + infiltrations[i].location.infiltrationData.maxClearanceLevel);
-            ns.tprint("          Starting security level: " + infiltrations[i].location.infiltrationData.startingSecurityLevel.toFixed(2));
+            ns.tprintf(infiltrations[i].location.name);
+            ns.tprintf("    City: " + infiltrations[i].location.city);
+            ns.tprintf("      Difficulty");
+            ns.tprintf("          Difficulty level: " + (infiltrations[i].difficulty * 33.3333).toFixed(0) + " / 100");
+            ns.tprintf("          Max clearance level: " + infiltrations[i].location.infiltrationData.maxClearanceLevel);
+            ns.tprintf("          Starting security level: " + infiltrations[i].location.infiltrationData.startingSecurityLevel.toFixed(2));
     
-            ns.tprint("\n");
+            ns.tprintf("\n");
         }
     } else {
         for (let i = 0; i < infiltrations.length; i++) {
-            ns.tprint(infiltrations[i].location.name);
-            ns.tprint("    City: " + infiltrations[i].location.city);
+            ns.tprintf(infiltrations[i].location.name);
+            ns.tprintf("    City: " + infiltrations[i].location.city);
             if (infiltrations[i].location.techVendorMaxRam > 0 && infiltrations[i].location.techVendorMinRam > 0) {
-                ns.tprint("    Max RAM: " + infiltrations[i].location.techVendorMaxRam + " GB");
-                ns.tprint("    Min RAM: " + infiltrations[i].location.techVendorMinRam + " GB");
+                ns.tprintf("    Max RAM: " + infiltrations[i].location.techVendorMaxRam + " GB");
+                ns.tprintf("    Min RAM: " + infiltrations[i].location.techVendorMinRam + " GB");
             }
-            ns.tprint("      Difficulty");
-            ns.tprint("          Difficulty level: " + (infiltrations[i].difficulty * 33.3333).toFixed(0) + " / 100");
-            ns.tprint("          Max clearance level: " + infiltrations[i].location.infiltrationData.maxClearanceLevel);
-            ns.tprint("          Starting security level: " + infiltrations[i].location.infiltrationData.startingSecurityLevel.toFixed(2));
+            ns.tprintf("      Difficulty");
+            ns.tprintf("          Difficulty level: " + (infiltrations[i].difficulty * 33.3333).toFixed(0) + " / 100");
+            ns.tprintf("          Max clearance level: " + infiltrations[i].location.infiltrationData.maxClearanceLevel);
+            ns.tprintf("          Starting security level: " + infiltrations[i].location.infiltrationData.startingSecurityLevel.toFixed(2));
     
-            ns.tprint("      Rewards");
-            ns.tprint("          Cash for selling: " + ns.nFormat(infiltrations[i].reward.sellCash, "$0.000a"));
-            ns.tprint("          Reputation for trading: " + ns.nFormat(infiltrations[i].reward.tradeRep, "0.000a"));
-            ns.tprint("          SoA Reputation: " + ns.nFormat(infiltrations[i].reward.SoARep, "0.000a"));
+            ns.tprintf("      Rewards");
+            ns.tprintf("          Cash for selling: " + ns.nFormat(infiltrations[i].reward.sellCash, "$0.000a"));
+            ns.tprintf("          Reputation for trading: " + ns.nFormat(infiltrations[i].reward.tradeRep, "0.000a"));
+            ns.tprintf("          SoA Reputation: " + ns.nFormat(infiltrations[i].reward.SoARep, "0.000a"));
     
-            ns.tprint("\n");
+            ns.tprintf("\n");
         }
     }
 
