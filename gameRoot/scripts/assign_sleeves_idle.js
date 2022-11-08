1c42e95f228e0f9e558888e16349f1596e2e6548
ahall1315 <aahall1315@gmail.com>
2022-11-08 17:03:36 -0500

    Added assign_sleeves_idle.js

---
+++ gameRoot/scripts/assign_sleeves_idle.js
@@ -0,0 +1,36 @@
+/** @param {import("NetscriptDefinitions").NS} ns */
+export async function main(ns) {
+    let numSleeves = ns.sleeve.getNumSleeves();
+    let error = false;
+    let count = 0;
+
+    let args = ns.flags([["help", false]]);
+    if (args.help) {
+        ns.tprintf("This script will attempt to assign all of your sleeves to idle/synchronize.");
+        ns.tprintf("The purpose of this script is build up bonus time for the sleeves.");
+        ns.tprintf("This is useful for making shock recovery faster.");
+        ns.tprintf("This script requires less RAM than assign_sleeves.js.");
+        return;
+    }
+    
+    for (let i = 0; i < numSleeves; i++) {
+        if (ns.sleeve.setToSynchronize(i)) {
+            ns.print("SUCCESS Sleeve " + i + " set to idle/synchronize.");
+            count++;
+        } else {
+            ns.print("ERROR Failed to set sleeve " + i + " to idle/synchronize.");
+            error = true;
+        }
+    }
+
+    if (error) {
+        ns.tprint("ERROR There was an error in assigning one or more of the sleeves.");
+    }
+
+    if (count === 0) {
+        ns.tprint("Failed to assign any sleeves. Do you have any sleeves?");
+    } else {
+        ns.tprint("Assigned " + count + " sleeves to idle/synchronize.");
+    }
+
+}
