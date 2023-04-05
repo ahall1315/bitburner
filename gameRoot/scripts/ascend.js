bafbe35186bfcd6699c1d2a4ccc3559a594866aa
Alex Hall <ahall2@augusta.edu>
2023-04-05 17:29:35 -0400

    Updated logging

--- gameRoot/scripts/ascend.js
+++ gameRoot/scripts/ascend.js
@@ -9,71 +9,93 @@ export async function main(ns) {
         ns.tprintf("Optional argument --equip to buy equipment for the gang member after ascending.");
         return;
     }
-    
-    const ASC_MULT_THRESH = 2 // Ascension multiplier threshold
+
+    const ASC_MULT_THRESH = 2; // Ascension multiplier threshold
     const EQUIP_MEMBERS_PATH = "/scripts/equip_members.js";
     let currentMembers = [];
-    let memberInfo = null;
     let gangInfo = null;
-    let res = null;
-    
+
     if (!ns.gang.inGang()) {
         ns.tprint("INFO You are not in a gang! You must join one before you can ascend gang members.");
         return;
     }
 
-    ns.disableLog("disableLog");
-    ns.disableLog("gang.setMemberTask");
-    ns.disableLog("gang.ascendMember");
-    
+    ns.disableLog("ALL");
+
     while (true) {
         currentMembers = ns.gang.getMemberNames();
         gangInfo = ns.gang.getGangInformation();
 
+        ns.print(`___________________________________________________
+        Ascension Multiplier Threshold: ${ASC_MULT_THRESH}
+        `);
+
         for (let i = 0; i < currentMembers.length; i++) {
-            res = ns.gang.getAscensionResult(currentMembers[i]);
-            memberInfo = ns.gang.getMemberInformation(currentMembers[i]);
+            currentMembers[i] = ns.gang.getMemberInformation(currentMembers[i])
+        }
+
+        for (let i = 0; i < currentMembers.length; i++) {
+            currentMembers[i].ascensionResult = ns.gang.getAscensionResult(currentMembers[i].name);
+
+            if (currentMembers[i].ascensionResult === undefined) {
+                ns.print(currentMembers[i].name + ": Cannot ascend");
+                continue;
+            }
+
+            // If the gang member can ascend
 
             if (gangInfo.isHacking) {
-                if (res.hack >= ASC_MULT_THRESH) {
-                    if (ns.gang.ascendMember(currentMembers[i]) === undefined) {
-                        ns.print("ERROR Failed to ascend gang member " + currentMembers[i]);
+                if (currentMembers[i].ascensionResult.hack >= ASC_MULT_THRESH) {
+                    if (ns.gang.ascendMember(currentMembers[i].name) === undefined) {
+                        ns.print("ERROR" + currentMembers[i].name + ": Failed to ascend gang member");
                     } else {
-                        ns.print("Successfully ascended " + currentMembers[i] + ". They lost " + ns.formatNumber(res.respect, 0) + " respect.");
+                        ns.print(currentMembers[i].name + ": Successfully ascended. They lost " + ns.formatNumber(currentMembers[i].ascensionResult.respect, 0) + " respect.");
                         if (args.train) {
-                            if (memberInfo.task !== "Train Hacking") {
-                                if (!ns.gang.setMemberTask(currentMembers[i], "Train Hacking")) {
-                                    ns.print("ERROR Failed to assign " + currentMembers[i] + " to train hacking");
+                            if (currentMembers[i].task !== "Train Hacking") {
+                                if (!ns.gang.setMemberTask(currentMembers[i].name, "Train Hacking")) {
+                                    ns.print("ERROR" + currentMembers[i].name + ": Failed to assign to train hacking");
                                 } else {
-                                    ns.print("Assigned " + currentMembers[i] + " to train hacking");
+                                    ns.print(currentMembers[i].name + ": Assigned to train hacking");
                                 }
                             }
                         }
                         if (args.equip) {
-                            ns.run(EQUIP_MEMBERS_PATH, 1, currentMembers[i], "-a");
+                            ns.run(EQUIP_MEMBERS_PATH, 1, currentMembers[i].name, "-a");
                         }
                     }
+                } else {
+                    ns.print(currentMembers[i].name + ": Can ascend. Multiplier is x" + ns.formatNumber(currentMembers[i].ascensionResult.hack, 5));
                 }
             } else {
-                // If combat gang
-                if (getAverage(ns, [res.str, res.def, res.dex, res.agi]) >= ASC_MULT_THRESH) {
-                    if (ns.gang.ascendMember(currentMembers[i]) === undefined) {
-                        ns.print("ERROR Failed to ascend gang member " + currentMembers[i]);
+                // If the gang is not a hacking gang, it is a combat gang
+
+                // Get the average of the multipliers for the ascension result and compare it to the ascension multiplier threshold
+                let avg = getAverage(ns, [
+                    currentMembers[i].ascensionResult.str,
+                    currentMembers[i].ascensionResult.def,
+                    currentMembers[i].ascensionResult.dex,
+                    currentMembers[i].ascensionResult.agi
+                ])
+                if (avg >= ASC_MULT_THRESH) {
+                    if (ns.gang.ascendMember(currentMembers[i].name) === undefined) {
+                        ns.print("ERROR" + currentMembers[i].name + ": Failed to ascend gang member");
                     } else {
-                        ns.print("Successfully ascended " + currentMembers[i] + ". They lost " + ns.formatNumber(res.respect, 0) + " respect.");
+                        ns.print(currentMembers[i].name + ": Successfully ascended. They lost " + ns.formatNumber(currentMembers[i].ascensionResult.respect, 0) + " respect.");
                         if (args.train) {
-                            if (memberInfo.task !== "Train Combat") {
-                                if (!ns.gang.setMemberTask(currentMembers[i], "Train Combat")) {
-                                    ns.print("ERROR Failed to assign " + currentMembers[i] + " to train combat");
+                            if (currentMembers[i].task !== "Train Combat") {
+                                if (!ns.gang.setMemberTask(currentMembers[i].name, "Train Combat")) {
+                                    ns.print("ERROR" + currentMembers[i].name + ": Failed to assign to train combat");
                                 } else {
-                                    ns.print("Assigned " + currentMembers[i] + " to train combat");
+                                    ns.print(currentMembers[i].name + ": Assigned to train combat");
                                 }
                             }
                         }
                         if (args.equip) {
-                            ns.run(EQUIP_MEMBERS_PATH, 1, currentMembers[i], "-a");
+                            ns.run(EQUIP_MEMBERS_PATH, 1, currentMembers[i].name, "-a");
                         }
                     }
+                } else {
+                    ns.print(currentMembers[i].name + ": Can ascend. Multiplier is x" + ns.formatNumber(avg, 5));
                 }
             }
         }
@@ -81,7 +103,6 @@ export async function main(ns) {
         await ns.sleep(5000); // sleep for 5 seconds
     }
 }
-
 
 export function autocomplete(data, args) {
     return [...data.servers]; // This script autocompletes the list of servers.
