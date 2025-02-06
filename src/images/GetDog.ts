c7e8644426312b3270ebe4a7f987cd19a84a16c9
Alex Hall <aahall1315@gmail.com>
2025-02-06 11:20:29 -0500

    Update GetCat.ts and add GetDog.ts

--- src/images/GetDog.ts
+++ src/images/GetDog.ts
@@ -1,4 +1,4 @@
-// Displays a random cat picture
+// Displays a random dog picture
 // Code modified from https://steamcommunity.com/sharedfiles/filedetails/?id=2683674829
 
 import { NS, AutocompleteData } from "@ns";
@@ -7,15 +7,15 @@ export async function main(ns: NS): Promise<void> {
     // Gets the directory of this script
     const scriptDir = ((p) => p ? p + "/" : "/")(ns.getScriptName().split("/").slice(0, -1).join("/"));
 
-    if (!await ns.wget("https://api.thecatapi.com/v1/images/search", scriptDir + "cat.txt")) {
+    if (!await ns.wget("https://dog.ceo/api/breeds/image/random", scriptDir + "dog.txt")) {
         ns.tprintf("ERROR Failed to fetch the image from the web. Are you connected to the internet?");
         ns.exit();
     }
 
-    let content = ns.read(scriptDir + "cat.txt");
-    let imageJSON = JSON.parse(content)[0];
+    let content = ns.read(scriptDir + "dog.txt");
+    let imageJSON = JSON.parse(content);
 
-    ns.alert(`<img src=\"${imageJSON.url}\" style="max-width: 1024px; max-height: 570px;"></img>`);
+    ns.alert(`<img src=\"${imageJSON.message}\" style="max-width: 1024px; max-height: 570px;"></img>`);
 }
 
 export function autocomplete(data: AutocompleteData, args: string[]): string[] {
